12e788f6a36394deda8921f4ec5a6b5c66a2789c
Marcos Lobo <142840907+lobomarcos@users.noreply.github.com>
2025-09-10 21:51:23 -0300

    ###

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# CONFIG/START DATABASE
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+db = SQLAlchemy(app)
+
+# DATABASE MODELING
+# rows - register | columns - infos
+# Product (rows) | id, name, price, description (colums)
+class Product (db.Model):
+    id = db.Column (db.Integer, primary_key = True)
+    name = db.Column (db.String(120), nullable = False)
+    price = db.Column (db.Float, nullable = False)
+    # "Text" does not have character limitation, like the string
+    description = db.column (db.Text, nullable = True)
 
 # ROOT - ROUTE - FUNCTION
 @app.route('/')
