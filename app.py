a80131205bab06353a5eee53843d57feff44a2e5
Marcos Lobo <142840907+lobomarcos@users.noreply.github.com>
2025-09-15 20:44:42 -0300

    ROUTES REC LIST REC PRODUCT AND UPDATE PRODUCT

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 
 # CONFIGURAÇÃO DO BANCO DE DADOS
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ecommerce.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # evita warning
+
 db = SQLAlchemy(app)
+CORS(app)
 
 # MODELAGEM DO BANCO
 # Tabela: Product
@@ -46,6 +49,45 @@ def delete_product(product_id):
     
     return jsonify ({'message': 'Product not found'}), 404
 
+# ROTA - RECUPERAÇÃO
+@app.route('/api/products/<int:product_id>', methods = ['GET'])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify({'id': product.id, 'name': product.name, 'price': product.price, 'description': product.description})
+    return jsonify({'message': 'Product not found'}), 404
+
+# ROTA - ATUALIZAÇÃO
+@app.route('/api/products/update/<int:product_id>', methods = ['PUT'])
+def update_product(product_id):
+    product = Product.query.get(product_id)
+    if not product:
+        return jsonify({'message': 'Product not found'}), 404
+    
+    data = request.json
+    if 'name' in data:
+        product.name = data['name']
+
+    if 'price' in data:
+        product.price = data['price']
+
+    if 'description' in data:
+        product.description = data['description']
+
+    db.session.commit()
+    return jsonify ({'message': 'Product updated successfully'})
+
+# ROTA - RECUPERAÇÃO DE LISTA DE PRODUTOS
+@app.route('/api/products', methods = ['GET'])
+def get_products():
+    products = Product.query.all()
+
+    product_list = []
+    for product in products:
+        product_data = ({'id': product.id, 'name': product.name, 'price': product.price})
+        product_list.append(product_data)
+    return jsonify(product_list)
+
 # ROTA/RAIZ PRINCIPAL - FUNÇÃO A SER EXECUTADA
 @app.route("/")
 def hello_world():
@@ -53,3 +95,5 @@ def hello_world():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+# PAREI EM 37 MIN
